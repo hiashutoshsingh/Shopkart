47440ecab7813e183634937227cd6bdf446963a4
ASHU <ashutoshsingh.0207@gmail.com>
2017-06-14 18:27:18 +0530

    women category added

--- app/src/main/java/com/shopkart/theinvincible/shopkart/WomenAccessories.java
+++ app/src/main/java/com/shopkart/theinvincible/shopkart/WomenAccessories.java
@@ -1,11 +1,19 @@
 package com.shopkart.theinvincible.shopkart;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,17 +21,109 @@ import android.view.ViewGroup;
  */
 public class WomenAccessories extends Fragment {
 
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
 
     public WomenAccessories() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_women_accessories, container, false);
+        View view = inflater.inflate(R.layout.fragment_women_accessories, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewAccessories);
+        recyclerView.setHasFixedSize(true);
+
+
+        mLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+
+        mAdapter = new GridAdapter();
+        recyclerView.setAdapter(mAdapter);
+        return view;
     }
 
+    public class GridAdapter extends RecyclerView.Adapter<GridAdapter.GridViewHolder> {
+
+        private List<WomenItem> womenItems;
+
+        public GridAdapter() {
+            womenItems = new ArrayList<>();
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+
+        }
+
+        @Override
+        public GridViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            Context context = viewGroup.getContext();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.layout_list_item, viewGroup, false);
+
+
+            return new GridViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(GridViewHolder gridViewHolder, int i) {
+            WomenItem womenItem = womenItems.get(i);
+            gridViewHolder.bindData(womenItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return womenItems.size();
+        }
+
+        public class GridViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView itemName;
+            private TextView itemPrice;
+            private ImageView itemImage;
+
+            public GridViewHolder(View itemView) {
+                super(itemView);
+                itemName = (TextView) itemView.findViewById(R.id.itemName);
+                itemPrice = (TextView) itemView.findViewById(R.id.itemPrice);
+                itemImage = (ImageView) itemView.findViewById(R.id.ImageWomenItem);
+
+            }
+
+            public void bindData(WomenItem womenItem) {
+                itemName.setText(womenItem.mitemName);
+                itemPrice.setText(womenItem.mitemPrice);
+                itemImage.setImageResource(womenItem.mitemImage);
+            }
+        }
+
+        public class WomenItem {
+            public String mitemName;
+            public String mitemPrice;
+            public int mitemImage;
+
+            public WomenItem(String mitemName, String mitemPrice, int mitemImage) {
+                this.mitemPrice = mitemPrice;
+                this.mitemName = mitemName;
+                this.mitemImage = mitemImage;
+            }
+        }
+
+
+    }
+
+
 }
+
--- app/src/main/java/com/shopkart/theinvincible/shopkart/WomenJewellery.java
+++ app/src/main/java/com/shopkart/theinvincible/shopkart/WomenJewellery.java
@@ -1,11 +1,19 @@
 package com.shopkart.theinvincible.shopkart;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +21,9 @@ import android.view.ViewGroup;
  */
 public class WomenJewellery extends Fragment {
 
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
 
     public WomenJewellery() {
         // Required empty public constructor
@@ -22,7 +33,98 @@ public class WomenJewellery extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_women_jewellery, container, false);
-    return view;}
+        View view = inflater.inflate(R.layout.fragment_women_jewellery, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewJewellery);
+        recyclerView.setHasFixedSize(true);
+
+
+        mLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+
+        mAdapter = new GridAdapter();
+        recyclerView.setAdapter(mAdapter);
+        return view;
+    }
+
+    public class GridAdapter extends RecyclerView.Adapter<GridAdapter.GridViewHolder> {
+
+        private List<WomenItem> womenItems;
+
+        public GridAdapter() {
+            womenItems = new ArrayList<>();
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+            womenItems.add(new WomenItem("Red Top", "$10", R.drawable.womenone));
+
+        }
+
+        @Override
+        public GridViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            Context context = viewGroup.getContext();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.layout_list_item, viewGroup, false);
+
+
+            return new GridViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(GridViewHolder gridViewHolder, int i) {
+            WomenItem womenItem = womenItems.get(i);
+            gridViewHolder.bindData(womenItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return womenItems.size();
+        }
+
+        public class GridViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView itemName;
+            private TextView itemPrice;
+            private ImageView itemImage;
+
+            public GridViewHolder(View itemView) {
+                super(itemView);
+                itemName = (TextView) itemView.findViewById(R.id.itemName);
+                itemPrice = (TextView) itemView.findViewById(R.id.itemPrice);
+                itemImage = (ImageView) itemView.findViewById(R.id.ImageWomenItem);
+
+            }
+
+            public void bindData(WomenItem womenItem) {
+                itemName.setText(womenItem.mitemName);
+                itemPrice.setText(womenItem.mitemPrice);
+                itemImage.setImageResource(womenItem.mitemImage);
+            }
+        }
+
+        public class WomenItem {
+            public String mitemName;
+            public String mitemPrice;
+            public int mitemImage;
+
+            public WomenItem(String mitemName, String mitemPrice, int mitemImage) {
+                this.mitemPrice = mitemPrice;
+                this.mitemName = mitemName;
+                this.mitemImage = mitemImage;
+            }
+        }
+
+
+    }
+
 
 }
+
